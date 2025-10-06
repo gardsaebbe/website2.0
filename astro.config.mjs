70e6cd975b7ac52fd0c97d23e57802575bf1b04a
Gard Sæbbe <g.saebbe@hotmail.com>
2025-10-07 01:23:40 +0200

    fortsatt work in progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   integrations: [react()],
